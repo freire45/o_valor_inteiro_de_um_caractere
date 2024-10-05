62ef918d79a3277ca27938870affcd77f4191056
Erick Freire <erilefr@gmail.com>
2024-10-05 13:10:41 -0300

    V1
    V1

---
+++ O-Valor-Inteiro-De-Um-Caractere/src/br/com/erickfreire/ovalorinteirodeumcaractere/O_Valor_Inteiro_De_Um_Caractere.java
@@ -0,0 +1,25 @@
+package br.com.erickfreire.ovalorinteirodeumcaractere;
+
+public class O_Valor_Inteiro_De_Um_Caractere {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Programa que exibe o valor inteiro de um caractere de acordo com a tabela ASCII: \n");
+		
+		System.out.printf("O caractere %c têm o valor %d%n", 'A', ((int) 'A'));
+		System.out.printf("O caractere %c têm o valor %d%n", 'B', ((int) 'B'));
+		System.out.printf("O caractere %c têm o valor %d%n", 'C', ((int) 'C'));
+		System.out.printf("O caractere %c têm o valor %d%n", 'a', ((int) 'a'));
+		System.out.printf("O caractere %c têm o valor %d%n", 'b', ((int) 'b'));
+		System.out.printf("O caractere %c têm o valor %d%n", 'c', ((int) 'c'));
+		System.out.printf("O caractere %c têm o valor %d%n", '0', ((int) '0'));
+		System.out.printf("O caractere %c têm o valor %d%n", '1', ((int) '1'));
+		System.out.printf("O caractere %c têm o valor %d%n", '2', ((int) '2'));
+		System.out.printf("O caractere %c têm o valor %d%n", '$', ((int) '$'));
+		System.out.printf("O caractere %c têm o valor %d%n", '*', ((int) '*'));
+		System.out.printf("O caractere %c têm o valor %d%n", '+', ((int) '+'));
+		System.out.printf("O caractere %c têm o valor %d%n", '/', ((int) '/'));
+		System.out.printf("O caractere %c têm o valor %d%n", ' ', ((int) ' '));
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# o_valor_inteiro_de_um_caractere
- O Valor Inteiro De Um Caractere
+# O Valor Inteiro De Um Caractere
+
+**O Valor Inteiro De Um Caractere**
+
+![O Valor Inteiro De Um Caractere](ovalorinteirodeumcaractere.png)
+
+Autor: Erick Leite Freire<br>
+Criado em: 05-10-2024<br>
+Site: [Erick Freire](https://www.erickfreire.com.br)<br>
